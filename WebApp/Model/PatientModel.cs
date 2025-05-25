c28feac2bfeb134ea51cceb459521477902d040c
Cristian <cristian.custodio@gmail.com>
2025-05-25 15:50:11 -0300

    Primeiro commit

---
+++ WebApp/Model/PatientModel.cs
@@ -0,0 +1,53 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorPatientRegistration.Models
+{
+    public class PatientModel
+    {
+        [Required(ErrorMessage = "O nome completo é obrigatório")]
+        public string Nome { get; set; }
+        
+        [Required(ErrorMessage = "A data de nascimento é obrigatória")]
+        public DateTime DataNascimento { get; set; } = DateTime.Now.AddYears(-30);
+        
+        [Required(ErrorMessage = "O CPF é obrigatório")]
+        [RegularExpression(@"^\d{3}\.\d{3}\.\d{3}-\d{2}$", ErrorMessage = "Formato de CPF inválido")]
+        public string CPF { get; set; }
+        
+        public string RG { get; set; }
+        
+        [Required(ErrorMessage = "O sexo é obrigatório")]
+        public string Sexo { get; set; }
+        
+        [EmailAddress(ErrorMessage = "Formato de e-mail inválido")]
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage = "O telefone é obrigatório")]
+        [RegularExpression(@"^\(\d{2}\) \d{5}-\d{4}$", ErrorMessage = "Formato de telefone inválido")]
+        public string Telefone { get; set; }
+        
+        [RegularExpression(@"^\d{5}-\d{3}$", ErrorMessage = "Formato de CEP inválido")]
+        public string CEP { get; set; }
+        
+        public string Endereco { get; set; }
+        
+        public string Numero { get; set; }
+        
+        public string Complemento { get; set; }
+        
+        public string Bairro { get; set; }
+        
+        public string Cidade { get; set; }
+        
+        public string Estado { get; set; }
+        
+        public string ConvenioId { get; set; }
+        
+        public string NumeroCarteira { get; set; }
+        
+        public string Alergias { get; set; }
+        
+        public string Observacoes { get; set; }
+    }
+}
